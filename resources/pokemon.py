ae56db2968b4acea7cb45780afc6149cad5a4669
Vincent <109750019+Yarkis01@users.noreply.github.com>
2023-02-19 02:10:35 +0100

    Formes regionales + diverses modifications (#16)
    * feat: Ajouts du support des formes régionales
    
    * Ajout des formes régionales d'Alola
    
    * add: Ajout des deux formes de Paldea
    
    * feat!: Ajout de la page 404 + modification de la structure des assets
    
    * add: Ajouts des formes régionales de Hisui
    
    * add: Ajouts formes régionales de Galar
    
    * fix: Correction de plusieurs erreurs
    
    * fix: Correction des valeurs d'expériences erronées
    
    * fix: Changement du message d'avertissement sur la page d'accueil
    
    ---------
    
    Co-authored-by: Ashzuu <116486394+Ashzuu@users.noreply.github.com>

--- resources/pokemon.py
+++ resources/pokemon.py
@@ -4,19 +4,17 @@ import json
 
 JSON_pokemon       = json.load(open("data/pokemon.json", encoding="utf8"))
 JSON_pokemon_to_id = json.load(open("data/pokemon_to_id.json", encoding="utf8"))
+JSON_forme_pokemon = json.load(open("data/formes_regionales.json", encoding="utf-8"))
 
 class Pokemon(Resource):
-    def get(self, pokemon: Union[str, int] = None) -> dict:
+    def get(self, pokemon: Union[str, int] = None, forme: str = None) -> dict:
         if pokemon is None:
             return JSON_pokemon
 
         try: pokemon_id = int(pokemon)
         except Exception:
             pokemon = str(pokemon).lower()
-            if pokemon in JSON_pokemon_to_id:
-                pokemon_id = JSON_pokemon_to_id[pokemon]
-            else:
-                pokemon_id = -1
+            pokemon_id = JSON_pokemon_to_id[pokemon] if pokemon in JSON_pokemon_to_id else -1
 
         if pokemon_id == -1 or pokemon_id > len(JSON_pokemon) - 1:
             return {
@@ -24,45 +22,31 @@ class Pokemon(Resource):
                 "message": "Impossible de trouver le Pokémon dans la base de données."
             }
         
-        return JSON_pokemon[pokemon_id]
+        if forme is None or forme not in JSON_forme_pokemon:
+            return JSON_pokemon[pokemon_id]
+        
+        return JSON_forme_pokemon[forme][str(pokemon_id)] if str(pokemon_id) in JSON_forme_pokemon[forme] else {"status": 404, "message": "Impossible de trouver la forme régionale du Pokémon demandé."}
 
 class Generation(Resource):
-    def get(self, gen: Union[str, int]) -> dict:
-        try:
-            gen = int(gen)
+    def get(self, gen: str = None) -> dict:
+        if gen is not None:
+            return [pkm for pkm in JSON_pokemon if str(pkm["generation"]) == gen and pkm["pokedexId"] != 0] or {
+                "status": 404,
+                "message": "Impossible d'afficher cette génération, car elle n'existe pas.",
+            }
 
-            data = [pkm for pkm in JSON_pokemon if pkm["generation"] == gen]
+        generations = {}
+        for pkm in JSON_pokemon:
+            if pkm["pokedexId"] == 0:
+                continue
 
-            if len(data) == 0:
-                data = {
-                    "status" : 404,
-                    "message": "Impossible d'afficher cette génération, car elle n'existe pas."
+            if pkm["generation"] not in generations:
+                generations[pkm["generation"]] = {
+                    "generation": pkm["generation"],
+                    "from": pkm["pokedexId"],
+                    "to": pkm["pokedexId"]
                 }
-        except Exception:
-            if gen.lower() == "list" or gen.lower() == "liste":
-                generations = {}
-                data = []
-
-                for pkm in JSON_pokemon:
-                    if pkm["pokedexId"] == 0:
-                        continue
-
-                    if pkm["generation"] not in generations:
-                        generations[pkm["generation"]] = {
-                            "generation": pkm["generation"],
-                            "from": pkm["pokedexId"],
-                            "to": pkm["pokedexId"]
-                        }
-                    else:
-                        generations[pkm["generation"]]["to"] = pkm["pokedexId"]
-
-                for gen in generations.values():
-                    data.append(gen)
-
             else:
-                data = {
-                    "status" : 404,
-                    "message": "Impossible d'afficher cette génération, car elle n'existe pas."
-                }
+                generations[pkm["generation"]]["to"] = pkm["pokedexId"]
 
-        return data
+        return list(generations.values())
