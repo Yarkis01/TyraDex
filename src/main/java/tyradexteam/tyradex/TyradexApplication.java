35928f2325682c824b3eb5009b8922cba2116c29
Ashzuu <116486394+Ashzuu@users.noreply.github.com>
2026-02-21 21:06:17 +0100

    refactor: Base de démarrage du projet avec un nouveau langage & de nouvelles technologies

---
+++ src/main/java/tyradexteam/tyradex/TyradexApplication.java
@@ -0,0 +1,13 @@
+package tyradexteam.tyradex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyradexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyradexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tyradexteam/tyradex/TyradexApplicationTests.java
@@ -0,0 +1,13 @@
+package tyradexteam.tyradex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyradexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
