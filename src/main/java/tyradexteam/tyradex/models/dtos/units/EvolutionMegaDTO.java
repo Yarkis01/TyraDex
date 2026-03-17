702fb5ec5ec40d3138276fe395d39c393dbe61f9
Ashzuu <116486394+Ashzuu@users.noreply.github.com>
2026-03-17 22:31:41 +0100

    feat/back -> Préparation à accueillir la gestion de la méga-évolution

--- src/main/java/tyradexteam/tyradex/models/dtos/units/EvolutionMegaDTO.java
+++ src/main/java/tyradexteam/tyradex/models/dtos/units/EvolutionMegaDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import tyradexteam.tyradex.models.dtos.NameDTO;
 
 /**
  * La classe EvolutionMegaDTO représente les informations spécifiques à l'évolution méga d'un personnage dans le jeu.
@@ -13,8 +14,24 @@ import lombok.Setter;
 @Setter
 @Builder
 public class EvolutionMegaDTO {
+    /**
+     * L'orbe nécessaire pour effectuer la méga évolution d'un Pokémon.
+     */
     @JsonProperty("orbe")
     private String orbe;
 
+    /**
+     * Le nom de la méga évolution, représenté par un objet NameDTO qui contient les noms de la méga évolution
+     * dans différentes langues (français, anglais, japonais).
+     */
+    @JsonProperty("name")
+    private NameDTO name;
+
+    /**
+     * Les sprites de la méga évolution, représentés par un objet SpriteUnitDTO qui contient les différentes images
+     * de la méga évolution (régulière, shiny, etc.).
+     */
+    @JsonProperty("sprites")
+    private SpriteUnitDTO sprites;
 
 }
