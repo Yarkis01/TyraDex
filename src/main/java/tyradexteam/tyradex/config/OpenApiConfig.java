76ca9451c342bb6139c85dbfbb383053ba12506d
Ashzuu <evan.dynak@bbox.fr>
2026-05-13 01:07:35 +0200

    fix: problems in the PR #91 reolve-conflicts -> dev

--- src/main/java/tyradexteam/tyradex/config/OpenApiConfig.java
+++ src/main/java/tyradexteam/tyradex/config/OpenApiConfig.java
@@ -8,12 +8,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    /**
+     * Configuration for the documentation of the API in OpenAPI & Swagger
+     * @return The OpenAPI configuration
+     */
     @Bean
     public OpenAPI info() {
         return new OpenAPI()
             .info(new Info()
             .title("Tyradex API")
-            .version("1.0.0")
+            .version("3.0.0")
             .description("API permettant de récupérer des données en rapport avec la licence Pokémon, orienté particulièrement sur les données des jeux-vidéos principaux de Pokémon.")
             .license(new License().name("MIT"))
         );
