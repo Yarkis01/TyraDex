23248c8cf03e9d5c3f19c4aad28772e92f96ffd7
Ashzuu <116486394+Ashzuu@users.noreply.github.com>
2026-05-13 12:50:45 +0200

    fix: allowed origins for API to give access to everyone

--- src/main/java/tyradexteam/tyradex/config/WebConfig.java
+++ src/main/java/tyradexteam/tyradex/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
