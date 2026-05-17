723362fa6d33539938acf2b6598cb7d39d2c05fe
Vincent P. <109750019+Yarkis01@users.noreply.github.com>
2026-05-17 17:53:48 +0200

    feat: use build version in OpenAPI and add build-info

--- src/main/java/tyradexteam/tyradex/config/OpenApiConfig.java
+++ src/main/java/tyradexteam/tyradex/config/OpenApiConfig.java
@@ -3,26 +3,30 @@ package tyradexteam.tyradex.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Class configuration for OpenAPI.
- */
 @Configuration
 public class OpenApiConfig {
-    /**
-     * Configuration for the documentation of the API in OpenAPI & Swagger
-     * @return The OpenAPI configuration
-     */
+
+    @Autowired
+    private BuildProperties buildProperties;
+
     @Bean
     public OpenAPI info() {
         return new OpenAPI()
+            .addServersItem(new Server()
+                .url("https://api.tyradex.app/v3")
+                .description("Tyradex - " + buildProperties.getVersion())
+            )
             .info(new Info()
-            .title("Tyradex API")
-            .version("3.0.0")
-            .description("API permettant de récupérer des données en rapport avec la licence Pokémon, orienté particulièrement sur les données des jeux-vidéos principaux de Pokémon.")
-            .license(new License().name("MIT"))
-        );
+                .title("Tyradex API")
+                .version(buildProperties.getVersion())
+                .description("API permettant de récupérer des données en rapport avec la licence Pokémon, orienté particulièrement sur les données des jeux-vidéos principaux de Pokémon.")
+                .license(new License().name("MIT"))
+            );
     }
 }
